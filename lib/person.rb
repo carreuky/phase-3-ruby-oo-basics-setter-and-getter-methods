195f00e984db303bb2ce925148aa4a332fdfff09
carreuky <carreuky@gmail.com>
2022-08-17 12:57:34 +0300

    commit tree 744601fd2cc804f6d184af4e648ee99e3fad02c6

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+ # setter method
+ def name=(name)
+    @name = name
+  end
 
+  # getter method
+  def name
+    @name
+  end
+
+  def job=(jobs)
+    @job = jobs
+  end
+
+  # getter method
+  def job
+    @job
+  end
 end
